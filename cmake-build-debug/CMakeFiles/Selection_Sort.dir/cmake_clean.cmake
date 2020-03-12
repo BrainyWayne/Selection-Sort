509ee9e8ea862d5ec1dcbd1d52a2892758664dda
brainywayne <eshunfrancisboateng@gmail.com>
2020-03-12 14:11:57 +0000

    Working code

---
+++ cmake-build-debug/CMakeFiles/Selection_Sort.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Selection_Sort.dir/main.cpp.o"
+  "Selection_Sort"
+  "Selection_Sort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Selection_Sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
